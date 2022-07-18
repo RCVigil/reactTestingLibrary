fb34735bf71ff3a5ba729785a0d46c3e122a2923
Rodrigo Cuervo <rodrigo_vigil@yahoo.com.br>
2022-07-18 16:13:47 -0300

    Teste o componente <App.js /> - parcial

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
 
 import App from '../App';
 
 describe('Teste o componente <App.js />', () => {
   test('O primeiro link deve possuir o texto Home', () => {
-    
+    render(<BrowserRouter><App /></BrowserRouter>);
+    const LinkComponentH = screen.getByRole('link', {
+      name: /Home/i,
+    });
+
+    const LinkComponentA = screen.getByRole('link', {
+      name: /About/i,
+    });
+
+    const LinkComponentF = screen.getByRole('link', {
+      name: /Favorite Pokémons/i,
+    });
+    expect(LinkComponentH).toBeInTheDocument();
+    expect(LinkComponentA).toBeInTheDocument();
+    expect(LinkComponentF).toBeInTheDocument();
   });
 });
