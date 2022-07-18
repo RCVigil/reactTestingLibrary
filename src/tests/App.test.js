8802f4637f5fd0a5c654031b8136a5efc6931c6b
Rodrigo Cuervo <rodrigo_vigil@yahoo.com.br>
2022-07-18 15:08:18 -0300

    [Rodrigo Cuervo] - Iniciando projeto React-Testing-Library

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,10 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+
+import App from '../App';
+
+describe('Teste o componente <App.js />', () => {
+  test('O primeiro link deve possuir o texto Home', () => {
+    
+  });
+});
