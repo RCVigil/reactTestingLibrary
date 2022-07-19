4cf094d3f0a2b23d468c6153a481bd1b20953b17
Rodrigo Cuervo <rodrigo_vigil@yahoo.com.br>
2022-07-18 23:52:10 -0300

    Exercício 3 - Teste o componente <FavoritePokemons.js />

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+
+import { render, screen } from '@testing-library/react';
+
+import { BrowserRouter } from 'react-router-dom';
+
+import { FavoritePokemons } from '../pages';
+
+describe('Teste o componente <FavoritePokemons.js />', () => {
+  test('Teste se é exibida na tela a mensagem No favorite pokemon found', () => {
+    render(<BrowserRouter><FavoritePokemons /></BrowserRouter>);
+    const notFavor = screen.getByText(/No favorite pokemon found/i);
+
+    expect(notFavor).toBeInTheDocument();
+  });
+});
