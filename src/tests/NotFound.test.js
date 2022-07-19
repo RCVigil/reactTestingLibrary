969d4da9b02cd14c24d28d9b3eb7d7a984a6fe74
Rodrigo Cuervo <rodrigo_vigil@yahoo.com.br>
2022-07-19 16:18:30 -0300

    Exercício 4 - Teste o componente <NotFound.js />

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,35 @@
-test('', () => {});
+import React from 'react';
+
+import { screen } from '@testing-library/react';
+
+import NotFound from '../pages/NotFound';
+
+import renderWithRouter from './RenderwithRouter';
+
+describe('Teste o componente <NotFound.js />', () => {
+  test('Teste se a página contém um heading h2 com o texto Page requested not', () => {
+    renderWithRouter(<NotFound />);
+
+    const componentNFound = screen.getByRole('heading', {
+      name: /Page requested not found/i,
+      level: 2,
+    });
+
+    expect(componentNFound).toBeInTheDocument();
+  });
+
+  test('Teste se a página mostra a imagem', () => {
+    renderWithRouter(<NotFound />);
+
+    const componentImage = screen.getByRole('img', {
+      name: /pikachu crying because the page requested was not found/i,
+    });
+
+    const imageSrc = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+
+    console.log(imageSrc);
+
+    // expect(componentImage).toBeInTheDocument();
+    expect(componentImage).toHaveAttribute('src', imageSrc);
+  });
+});
--- src/tests/RenderwithRouter.js
+++ src/tests/RenderwithRouter.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import { Router } from 'react-router-dom';
-import createMemoryHistory from 'history';
+import { createMemoryHistory } from 'history';
 import { render } from '@testing-library/react';
 
-function renderWithRouter(componentRender) {
+function renderWithRouter(component) {
   const customHistory = createMemoryHistory();
 
-  const renderReturn = render(
-    <Router history={ customHistory }>{componentRender}</Router>,
+  const returnFromRender = render(
+    <Router history={ customHistory }>
+      {component}
+    </Router>,
   );
 
   return {
-    ...renderReturn,
     history: customHistory,
+    ...returnFromRender,
   };
 }
 
