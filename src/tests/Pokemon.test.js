090f87f35aacf20b3970d1be8ac3e19c25adb7e3
Rodrigo Cuervo <rodrigo_vigil@yahoo.com.br>
2022-12-28 22:20:23 -0300

    finalizando trybe

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,9 @@
-test('', () => {});
+import React from 'react';
+import App from '../App';
+import renderWithRouter from './RenderwithRouter';
+
+describe('Teste o componente <Pokemon.js />', () => {
+  test('Teste se é renderizado um card com as informações de determinado pokémon', () => {
+    renderWithRouter(<App />);
+  });
+});
